7e77c03d9198a80aee80552316dbb7048202c33a
rinhosero <146875807+rinhosero@users.noreply.github.com>
2023-11-18 22:16:03 +0500

    Add files via upload

---
+++ +page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Registr from '$lib/components/registr.svelte';
+</script>
+<div class="wrapped">
+	<Registr />
+</div>
+
+
+<style>
+	.wrapped {
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	} 
+	/* .background{
+	min-height: 100%;
+	min-width: 640px;
+	width: 100%;
+	height: auto;
+
+	position: fixed;
+	top: 0;
+	left: 0;
+	} */
+</style>
